bf0c9fdb55a740611d02558e8df0189c387ab9c3
Jan Juraszek <janj@telestream.net>
2017-04-19 15:51:42 +0200

    Multi-chunk upload

--- src/main/java/cloud/UploadSession.java
+++ src/main/java/cloud/UploadSession.java
@@ -9,6 +9,8 @@ import utils.GsonHelper;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.util.HashMap;
@@ -27,6 +29,7 @@ public class UploadSession {
     private static final String KEY_FILE_SIZE = "file_size";
     private static final String KEY_FILE_NAME = "file_name";
     private static final String KEY_PROFILES = "profiles";
+    private static final String KEY_MULTI_CHUNK = "multi_chunk";
     private static final String DEFAULT_PROFILE = "h264";
 
     private static final String PATH_VIDEOS_UPLOAD = "/videos/upload";
@@ -35,6 +38,11 @@ public class UploadSession {
     private long fileSize;
 
     private String location;
+    private int parts;
+    private int part_size;
+    private int max_connections;
+    
+    private int missing_parts[];
 
     public UploadStatus uploadStatus;
     private Video video;
@@ -60,6 +68,7 @@ public class UploadSession {
         uploadParams.put(KEY_FILE_SIZE, fileSize);
         uploadParams.put(KEY_FILE_NAME, file.getName());
         uploadParams.put(KEY_PROFILES, DEFAULT_PROFILE);
+        uploadParams.put(KEY_MULTI_CHUNK, true);
 
         if(params != null) uploadParams.putAll(params);
 
@@ -73,6 +82,9 @@ public class UploadSession {
 
         if(response != null) {
             uploadSession.location = GsonHelper.get().fromJson(response, VideoUploadResponse.class).location;
+            uploadSession.parts = GsonHelper.get().fromJson(response, VideoUploadResponse.class).parts;
+            uploadSession.part_size = GsonHelper.get().fromJson(response, VideoUploadResponse.class).part_size;
+            uploadSession.max_connections = GsonHelper.get().fromJson(response, VideoUploadResponse.class).max_connections;
         }
 
         uploadSession.uploadStatus = UploadStatus.INITIALIZED;
@@ -80,39 +92,41 @@ public class UploadSession {
     }
 
     public void start() throws IOException {
-        startFromPosition(0);
-    }
-
-    private void startFromPosition(long position) throws IOException {
         if(uploadStatus != UploadStatus.INITIALIZED)
             throw new IllegalStateException("Already started");
 
         uploadStatus = UploadStatus.UPLOADING;
 
-        byte[] buffer = new byte[CHUNK_SIZE];
-        long counter = 0;
-        long index;
-        int bytesRead;
+        for(int i = 0; i<parts; ++i) {
+            sendPart(i);
+        }
 
+        while(isRemoteStatusOk()==false)
+        {
+            retry();
+        }
+    }
+
+    private void sendPart(int part_id) throws IOException {
         try {
+            byte[] buffer = new byte[part_size];
+            long position = (long)part_size * (long)part_id;
+
             FileInputStream inputStream = new FileInputStream(file);
             inputStream.getChannel().position(position);
+            int bytesRead = inputStream.read(buffer, 0, part_size);
 
-            while ((bytesRead = inputStream.read(buffer, 0, CHUNK_SIZE)) != -1) {
-                index = counter * (long)CHUNK_SIZE;
+            if(bytesRead == -1) {
+                // EOF actually shouldn't happen - part_id must be wrong
+                uploadStatus = UploadStatus.ERROR;
+                return;
+            }
 
-                ChunkUploader.HttpResponse response = new ChunkUploader(location, position + index,
-                        position + index + CHUNK_SIZE-1, fileSize, bytesRead).uploadChunk(buffer);
+            ChunkUploader.HttpResponse response = new ChunkUploader(location, part_id,
+                    fileSize, bytesRead).uploadChunk(buffer);
 
-                int responseStatus = response.getStatus();
-                if (responseStatus == HttpURLConnection.HTTP_OK) {
-                    uploadStatus = UploadStatus.UPLOADED;
-                    this.video = gson.fromJson(response.getBody(), VideoService.TYPE_VIDEO);
-                } else if (responseStatus != HttpURLConnection.HTTP_NO_CONTENT) {
-                    this.uploadStatus = UploadStatus.ERROR;
-                    break;
-                }
-                counter++;
+            if(response.getStatus()!=HttpURLConnection.HTTP_OK) {
+                uploadStatus = UploadStatus.ERROR;
             }
         } catch (IOException e) {
             this.uploadStatus = UploadStatus.ERROR;
@@ -120,27 +134,42 @@ public class UploadSession {
             throw e;
         }
     }
+    
+    private boolean isRemoteStatusOk() throws IOException {
+        URL url = new URL(location);
+        HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+        conn.setRequestMethod("GET");
+        conn.connect();
+        BufferedReader br = new BufferedReader(new InputStreamReader((conn.getInputStream())));
+        StringBuilder respBody = new StringBuilder();
+        String output;
+        while ((output = br.readLine()) != null) {
+            respBody.append(output);
+        }
+        missing_parts = GsonHelper.get().fromJson(respBody.toString(), MissingChunksResponse.class).missing_parts;
+
+        if(missing_parts.length==0) {
+            uploadStatus = UploadStatus.UPLOADED;
+            return true;
+        }
+
+        return false;
+    }
+
+    private void retry() throws IOException {
+        for(int i=0; i<missing_parts.length; ++i)
+        {
+            sendPart(missing_parts[i]);
+        }
+    }
 
     public void resume() throws IOException {
         if(uploadStatus == UploadStatus.UPLOADED)
             throw new IllegalStateException("Already uploaded");
-
-        URL url = new URL(location);
-        HttpURLConnection httpConn = (HttpURLConnection) url.openConnection();
-        httpConn.setRequestMethod("POST");
-
-        httpConn.setRequestProperty("Content-Type", "application/octet-stream");
-        httpConn.setUseCaches(false);
-        httpConn.setRequestProperty("Content-Range", "bytes */" + fileSize);
-        httpConn.setRequestProperty("Content-Length", "0");
-        httpConn.setRequestProperty("Content-Transfer-Encoding", "binary");
-
-        httpConn.connect();
-        String rangeHeader = httpConn.getHeaderField("Range");
-        if(rangeHeader != null) {
-            String rangeEnd = rangeHeader.split("-")[1];
-            uploadStatus = UploadStatus.INITIALIZED;
-            startFromPosition(Long.valueOf(rangeEnd));
+        
+        if(isRemoteStatusOk()==false)
+        {
+            retry();
         }
     }
 
@@ -173,5 +202,13 @@ public class UploadSession {
     private static class VideoUploadResponse {
         private String id;
         private String location;
+        private int parts;
+        private int part_size;
+        private int max_connections;
+        private String extra_files;
+    }
+    
+    private static class MissingChunksResponse {
+        private int missing_parts[];
     }
 }
