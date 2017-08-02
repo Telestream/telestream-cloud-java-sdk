47ebca50fce89959fe85c6e36de5b6ca3f27f30a
Dominik Cencek <dominikc@telestream.net>
2017-08-02 16:56:25 +0200

    Fix same_as_source float value

--- src/main/java/net/telestream/cloud/utils/GsonHelper.java
+++ src/main/java/net/telestream/cloud/utils/GsonHelper.java
@@ -1,17 +1,32 @@
 package net.telestream.cloud.utils;
 
-import com.google.gson.FieldNamingPolicy;
-import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
+import com.google.gson.*;
+
+import java.lang.reflect.Type;
 
 /**
  * Created by maciejwitowski on 1/28/16.
  */
 public class GsonHelper {
+    static class FloatDeserializer implements JsonDeserializer<Float>
+    {
+        @Override
+        public Float deserialize(JsonElement json, Type typeOfT, JsonDeserializationContext context)
+                throws JsonParseException
+        {
+            String value = json.getAsString();
+
+            if (value.equals("same_as_source")) {
+                return new Float(-1.0);
+            }
+            return Float.parseFloat(value);
+        }
+    }
 
     static final Gson GSON = new GsonBuilder().serializeNulls()
             .setDateFormat("yyyy/MM/dd HH:mm:ss Z")
             .setFieldNamingPolicy(FieldNamingPolicy.LOWER_CASE_WITH_UNDERSCORES)
+            .registerTypeAdapter(Float.class, new FloatDeserializer())
             .create();
 
     public static Gson get() {
